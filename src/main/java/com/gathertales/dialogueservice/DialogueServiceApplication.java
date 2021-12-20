2180b42a9ef204c2c191dbc0bf201566ecb088fa
Tiago Cabral <1412695+cabral-tiago@users.noreply.github.com>
2021-12-20 17:34:21 +0000

    [Upload] Initial Upload

---
+++ src/main/java/com/gathertales/dialogueservice/DialogueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gathertales.dialogueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialogueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DialogueServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gathertales/dialogueservice/DialogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gathertales.dialogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
