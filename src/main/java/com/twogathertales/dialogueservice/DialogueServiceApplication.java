e69475b5439c634575338f028db5872bdfa47e47
Tiago Cabral <1412695+cabral-tiago@users.noreply.github.com>
2021-12-26 14:07:33 +0000

    [Refactor] Renaming package
    Renaming "gathertales" to "twogathertales", as the
    "2" got omitted.

--- src/main/java/com/twogathertales/dialogueservice/DialogueServiceApplication.java
+++ src/main/java/com/twogathertales/dialogueservice/DialogueServiceApplication.java
@@ -1,4 +1,4 @@
-package com.gathertales.dialogueservice;
+package com.twogathertales.dialogueservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/twogathertales/dialogueservice/DialogueServiceApplicationTests.java
+++ src/test/java/com/twogathertales/dialogueservice/DialogueServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gathertales.dialogueservice;
+package com.twogathertales.dialogueservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
