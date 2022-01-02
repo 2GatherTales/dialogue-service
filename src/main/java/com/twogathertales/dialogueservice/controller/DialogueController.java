02a21b79ff1132459cf800b33ae87a15706bf4c3
Tiago Cabral <1412695+cabral-tiago@users.noreply.github.com>
2022-01-02 14:48:03 +0000

    [Add] "Event" GET Request Mapping

--- src/main/java/com/twogathertales/dialogueservice/controller/DialogueController.java
+++ src/main/java/com/twogathertales/dialogueservice/controller/DialogueController.java
@@ -1,11 +1,12 @@
 package com.twogathertales.dialogueservice.controller;
 
+import com.twogathertales.dialogueservice.model.event.Event;
+import com.twogathertales.dialogueservice.services.implementation.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeTypeUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -13,6 +14,8 @@ import java.util.Calendar;
 @RestController
 @RequestMapping("api/dialogue")
 public class DialogueController {
+    @Autowired
+    private EventService eventService;
 
     public static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
 
@@ -32,6 +35,18 @@ public class DialogueController {
             return new ResponseEntity<String>(response, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @RequestMapping(value = "event/{eventid}",
+            method = RequestMethod.GET,
+            produces = {MimeTypeUtils.APPLICATION_JSON_VALUE},
+            headers = "Accept=application/json")
+    public ResponseEntity<Event> event(@PathVariable("eventid") Long id){
+        try{
+            return new ResponseEntity<Event>(eventService.find(id), HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<Event>(HttpStatus.BAD_REQUEST);
         }
     }
 
